6e3b536b826e60253a5668a118a78c01fb38f647
Ushio Shugo <ushio.s@gmail.com>
2017-08-25 01:11:28 +0900

    Using property in nested.

--- template.go
+++ template.go
@@ -32,53 +32,23 @@ type All struct {
 
 // Property .
 type Property struct {
-	All       *All                `json:"_all,omitempty"`
-	Type      string              `json:"type,omitempty"`
-	Format    string              `json:"format,omitempty"`
-	FieldData string              `json:"fielddata,omitempty"`
-	Fields    map[string]Field    `json:"fields,omitempty"`
-	Properies map[string]Property `json:"properties,omitempty"`
+	All         *All                `json:"_all,omitempty"`
+	Type        PropertyType        `json:"type,omitempty"`
+	Format      string              `json:"format,omitempty"`
+	FieldData   bool                `json:"fielddata,omitempty"`
+	Fields      map[string]Property `json:"fields,omitempty"`
+	Properies   map[string]Property `json:"properties,omitempty"`
+	IgnoreAbove int                 `json:"ignore_above,omitempty"`
 }
 
-// Field .
-type Field struct {
-	Type        string `json:"type"`
-	IgnoreAbove int    `json:"ignore_above"`
-}
+// PropertyType .
+type PropertyType string
 
-// // MarshalJSON for json marshaler
-// func (m Mappings) MarshalJSON() ([]byte, error) {
-// 	b := bytes.Buffer{}
-//
-// 	if _, err := b.WriteString("{"); err != nil {
-// 		return nil, err
-// 	}
-//
-// 	l := len(m.Type)
-// 	c := 0
-// 	for k, i := range m.Type {
-// 		if _, err := b.WriteString(fmt.Sprintf("\"%s\":", k)); err != nil {
-// 			return nil, err
-// 		}
-//
-// 		jb, err := json.Marshal(i)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-//
-// 		if _, err := b.Write(jb); err != nil {
-// 			return nil, err
-// 		}
-//
-// 		c = c + 1
-// 		if c != l {
-// 			b.WriteString(",")
-// 		}
-// 	}
-//
-// 	if _, err := b.WriteString("}"); err != nil {
-// 		return nil, err
-// 	}
-//
-// 	return b.Bytes(), nil
-// }
+const (
+	PropertyTypeDate     PropertyType = "date"
+	PropertyTypeInteger  PropertyType = "integer"
+	PropertyTypeText     PropertyType = "text"
+	PropertyTypeGeoPoint PropertyType = "get_point"
+	PropertyTypeNested   PropertyType = "nested"
+	PropertyTypeKeyword  PropertyType = "keyword"
+)
--- template_test.go
+++ template_test.go
@@ -6,46 +6,86 @@ import (
 	"testing"
 )
 
-// func TestMappingsMarshalJSON(t *testing.T) {
-// 	table := []struct {
-// 		mappings Mappings
-// 		out      string
-// 	}{
-// 		{
-// 			mappings: Mappings{
-// 				Type: map[string]Type{
-// 					"type1": Type{
-// 						Source: Source{
-// 							Enabled: false,
-// 						},
-// 						Properties: map[string]Property{
-// 							"page": Property{
-// 								Type: "integer",
-// 							},
-// 						},
-// 					},
-// 				},
-// 			},
-// 			out: "{}",
-// 		},
-// 	}
-//
-// 	for _, row := range table {
-// 		_, err := json.Marshal(row.mappings)
-// 		if err != nil {
-// 			t.Fatalf("json.Marshal got error: %s", err)
-// 		}
-//
-// 		m := Mappings{}
-// 		if err := json.Unmarshal([]byte(row.out), &m); err != nil {
-// 			t.Fatalf("json.Unmarshal got error: %s", err)
-// 		}
-//
-// 		if !reflect.DeepEqual(row.mappings, m) {
-// 			t.Errorf("deep equal missed. expected(%v) but (%b)", row.mappings, m)
-// 		}
-// 	}
-// }
+func TestMappingsMarshalJSON(t *testing.T) {
+	table := []struct {
+		mappings map[string]Type
+		out      string
+	}{
+		{
+			mappings: map[string]Type{
+				"_defualt_": Type{
+					All: &All{
+						Enabled: true,
+					},
+				},
+				"some_type": Type{
+					Properties: map[string]Property{
+						"@timestamp": Property{
+							Type:   PropertyTypeDate,
+							Format: "yyyy-MM-dd'T'HH:mm:ssZ",
+						},
+						"count": Property{
+							Type: PropertyTypeInteger,
+						},
+						"location": Property{
+							Type: PropertyTypeGeoPoint,
+						},
+						"word": Property{
+							Type:      PropertyTypeText,
+							FieldData: true,
+							Fields: map[string]Property{
+								"keyword": Property{
+									Type:        PropertyTypeKeyword,
+									IgnoreAbove: 256,
+								},
+							},
+						},
+						"object": Property{
+							Type: PropertyTypeNested,
+							Properies: map[string]Property{
+								"title": Property{
+									Type:        PropertyTypeKeyword,
+									IgnoreAbove: 256,
+								},
+								"user": Property{
+									Type: PropertyTypeNested,
+									Properies: map[string]Property{
+										"first_name": Property{
+											Type: PropertyTypeKeyword,
+										},
+										"last_name": Property{
+											Type: PropertyTypeKeyword,
+										},
+										"age": Property{
+											Type: PropertyTypeInteger,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			out: `{"_defualt_":{"_all":{"enabled":true}},"some_type":{"properties":{"@timestamp":{"type":"date","format":"yyyy-MM-dd'T'HH:mm:ssZ"},"count":{"type":"integer"},"location":{"type":"get_point"},"object":{"type":"nested","properties":{"title":{"type":"keyword","ignore_above":256},"user":{"type":"nested","properties":{"age":{"type":"integer"},"first_name":{"type":"keyword"},"last_name":{"type":"keyword"}}}}},"word":{"type":"text","fielddata":true,"fields":{"keyword":{"type":"keyword","ignore_above":256}}}}}}`,
+		},
+	}
+
+	for _, row := range table {
+		_, err := json.Marshal(row.mappings)
+		if err != nil {
+			t.Fatalf("json.Marshal got error: %s", err)
+		}
+
+		m := map[string]Type{}
+		if err := json.Unmarshal([]byte(row.out), &m); err != nil {
+			t.Fatalf("json.Unmarshal got error: %s", err)
+		}
+
+		if !reflect.DeepEqual(row.mappings, m) {
+			t.Errorf("deep equal missed. expected(%v) but (%v)", row.mappings, m)
+		}
+	}
+}
 
 func TestTemplateMarshalJSON(t *testing.T) {
 	table := []struct {
@@ -90,7 +130,6 @@ func TestTemplateMarshalJSON(t *testing.T) {
 			t.Fatalf("json.Unmarshal got error: %s", err)
 		}
 
-		// log.Println("out:", string(b))
 		if !reflect.DeepEqual(row.template, tpl) {
 			t.Errorf("deep equal missed. expected(%v) but (%v)", row.template, tpl)
 		}
