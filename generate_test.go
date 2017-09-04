753c377aad807a0011693f93b8f9a6c1add1219b
Ushio Shugo <ushio.s@gmail.com>
2017-09-04 12:20:30 +0900

    Fixed test.

--- generate_test.go
+++ generate_test.go
@@ -2,7 +2,6 @@ package elit
 
 import (
 	"encoding/json"
-	"fmt"
 	"reflect"
 	"testing"
 )
@@ -75,7 +74,7 @@ func TestGenerate(t *testing.T) {
 	}{
 		{
 			input:  SampleModel{},
-			result: `{"Empty":{"type":"text"},"base_base_sample_model_live":{"type":"integer"},"base_sample_model_name":{"type":"text"},"geo":{"type":"geo_point"},"id":{"type":"integer"},"int_list":{"type":"integer"},"normal":{"type":"text"},"ok":{"type":"boolean"},"omit_empty":{"type":"text"},"point":{"type":"integer"},"psub":{"type":"boolean"},"second":{"type":"float"},"small_second":{"type":"float"},"string_list":{"type":"text"},"sub":{"type":"nested","properties":{"":{"type":"integer"},"Child":{"type":"nested","properties":{"name":{"type":"text"}}},"base_sub_model_title":{"type":"text"},"body":{"type":"text"}}}}`,
+			result: `{"Empty":{"type":"text","fielddata":true,"fields":{"words":{"type":"keyword"}}},"base_base_sample_model_live":{"type":"integer"},"base_sample_model_name":{"type":"text","fielddata":true,"fields":{"words":{"type":"keyword"}}},"geo":{"type":"geo_point"},"id":{"type":"integer"},"int_list":{"type":"integer"},"normal":{"type":"text","fielddata":true,"fields":{"words":{"type":"keyword"}}},"ok":{"type":"boolean"},"omit_empty":{"type":"text","fielddata":true,"fields":{"words":{"type":"keyword"}}},"point":{"type":"integer"},"psub":{"type":"boolean"},"second":{"type":"float"},"small_second":{"type":"float"},"string_list":{"type":"text","fielddata":true,"fields":{"words":{"type":"keyword"}}},"sub":{"type":"nested","properties":{"":{"type":"integer"},"Child":{"type":"nested","properties":{"name":{"type":"text","fielddata":true,"fields":{"words":{"type":"keyword"}}}}},"base_sub_model_title":{"type":"text","fielddata":true,"fields":{"words":{"type":"keyword"}}},"body":{"type":"text","fielddata":true,"fields":{"words":{"type":"keyword"}}}}}}`,
 		},
 	}
 
@@ -95,8 +94,8 @@ func TestGenerate(t *testing.T) {
 			t.Errorf("json.Unmarshal got error: %s", err)
 		}
 
-		b, _ := json.Marshal(propertyMap)
-		fmt.Println(string(b))
+		// b, _ := json.Marshal(propertyMap)
+		// fmt.Println(string(b))
 
 		if !reflect.DeepEqual(res, propertyMap) {
 			t.Errorf("result map expected (%v) but (%v)", res, propertyMap)
