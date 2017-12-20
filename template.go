b8f78fcca10db9901bc3302f9ed41ec879b02052
ushio <ushio.s@gmail.com>
2017-12-20 11:24:26 +0900

    Add analyzer struct

--- template.go
+++ template.go
@@ -9,8 +9,15 @@ type Template struct {
 
 // Settings node settings
 type Settings struct {
-	NumberOfShards   uint `json:"number_of_shards"`
-	NumberOfReplicas uint `json:"number_of_replicas,omitempty"`
+	NumberOfShards   uint     `json:"number_of_shards"`
+	NumberOfReplicas uint     `json:"number_of_replicas,omitempty"`
+	Analysis         Analysis `json:"analysis,omitempty"`
+}
+
+// Analysis settings
+type Analysis struct {
+	Analyzer map[string]interface{} `json:"analyzer,omitempty"`
+	Filter   map[string]interface{} `json:"filter,omitempty"`
 }
 
 // Type .
